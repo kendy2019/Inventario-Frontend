be6a1ab8c6d76e553be3235139f7bb8eb3721822
Joey <joel.quispe.vilca147@gmail.com>
2025-10-12 13:54:28 -0500

    Api corregido

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,6 @@ import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
-export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
 
 export default function RootLayout({
   children,
--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -88,7 +88,7 @@ export function LoginForm() {
       // FIN DE CREDENCIALES DE PRUEBA
       // ============================================================================
 
-      const response = await fetch("http://localhost:8080/api/login", {
+      const response = await fetch("http://localhost:8080/api/auth/login", { //Segun la api el controlador de SpringBoot
         method: "POST",
         headers: {
           "Content-Type": "application/json",
